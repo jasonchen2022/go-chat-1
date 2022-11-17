7bfb62e68c6cb772df1408329218fbd4cf87bb5f
kk <Klein66593@gmail.com>
2022-11-17 16:48:26 +0800

    Update talk.go

--- internal/entity/talk.go
+++ internal/entity/talk.go
@@ -25,7 +25,7 @@ const (
 	MsgTypeSystemText    = 0  // 系统文本消息
 	MsgTypeText          = 1  // 文本消息
 	MsgTypeFile          = 2  // 文件消息
-	MsgTypeForward       = 3  // 会话消息
+	MsgTypeForward       = 3  // 会话转发消息
 	MsgTypeCode          = 4  // 代码消息
 	MsgTypeVote          = 5  // 投票消息
 	MsgTypeGroupNotice   = 6  // 群组公告
