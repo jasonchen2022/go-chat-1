6e18ceac4d8511d1799699645748ad89aaa6b139
kk <Klein66593@gmail.com>
2022-11-17 22:25:31 +0800

    up

--- internal/http/internal/handler/web/v1/talk/talk_message.go
+++ internal/http/internal/handler/web/v1/talk/talk_message.go
@@ -137,6 +137,7 @@ func (c *Message) Answer(ctx *ichat.Context) error {
 		TalkType:   params.TalkType,
 		ReceiverId: params.ReceiverId,
 		RecordId:   params.RecordId,
+		Text:       params.Text,
 	})
 	if err != nil {
 		return ctx.BusinessError(err.Error())
